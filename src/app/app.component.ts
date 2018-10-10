45a46dc340b0129351a63c027ff415f44d8d99e1
datsgawas <dattaram.gawas@metamagic.in>
2018-10-10 11:34:37 +0530

    app updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {ApplicationRef, Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {filter, map} from 'rxjs/operators';
+import {Observable, of} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,72 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rxjsObservable';
+  countryData: any;
+  countryData$: Observable<any>;
+
+  datareader = 'response.data';
+
+  constructor(private _httpClient: HttpClient) {
+
+  }
+
+  getCountry() {
+  this.countryData$ = this._httpClient.get('assets/data/country.json').pipe(
+      map(
+        x => {
+          return this.getResponseData(x);
+        }
+      )
+    )
+    this.countryData$.subscribe(
+      res=>{
+        debugger;
+        this.countryData = res;
+      }
+    )
+
+  }
+
+  getResponseData(httpResponse: any) {
+    let responsedata = httpResponse;
+    if (this.datareader != null) {
+      const dr = this.datareader.split('.');
+      for (const ir of dr) {
+        responsedata = responsedata[ir];
+      }
+    } else {
+      responsedata = httpResponse;
+    }
+
+    return responsedata;
+  }
+
+  findCountry(event: any){
+
+    if(event.target.value != '') {
+      this.getFiterData(event.target.value).subscribe(
+        res =>{
+          this.countryData = res;
+        }
+      )
+    } else {
+      this.countryData$.subscribe(
+        res=>{
+          debugger;
+          this.countryData = res;
+        }
+      )
+    }
+
+
+
+  }
+  getFiterData( key: string) {
+   return this.countryData$.pipe(
+      map( result =>  result.filter(one => one.countryCode1 == key)
+      )
+    )
+  }
 }
+
+
